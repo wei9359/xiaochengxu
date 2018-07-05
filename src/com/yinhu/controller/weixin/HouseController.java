6d3e46d7b61e800d73f4721c8fa1eb73441261d8
wei9359 <892958547@qq.com>
2018-07-05 14:32:15 +0800

    测试提交

--- src/com/yinhu/controller/weixin/HouseController.java
+++ src/com/yinhu/controller/weixin/HouseController.java
@@ -194,7 +194,7 @@ public class HouseController {
     * @return Message
     */
     @RequestMapping(value = "addHouse")
-    public @ResponseBody Message addHouse(String userID,List<Map<String,String>> houseParts, int price, int mztype, String houseDescribe, int houseSize, String houseLocal,String[] houseImgs,String houseLayout,String housezx,String houselc,int housecw,String housekf,int zffkfs,int mffkfs,int langitude,int latitude,int BDType,String province,String city,String county){
+    public @ResponseBody Message addHouse(String userID,Object houseParts, int price, int mztype, String houseDescribe, int houseSize, String houseLocal,String[] houseImgs,String houseLayout,String housezx,String houselc,int housecw,String housekf,int zffkfs,int mffkfs,int langitude,int latitude,int BDType,String province,String city,String county){
         logger.info("添加房屋信息");
         if(!StringUtil.isEmpty(userID) && !StringUtil.isEmpty(houseDescribe) && !StringUtil.isEmpty(houseLocal) && houseImgs.length==0 && !StringUtil.isEmpty(houseLayout) && !StringUtil.isEmpty(housezx) && !StringUtil.isEmpty(houselc) && !StringUtil.isEmpty(housekf) && !StringUtil.isEmpty(province) && !StringUtil.isEmpty(city) && !StringUtil.isEmpty(county) && houseParts!=null){
             try {
@@ -232,15 +232,15 @@ public class HouseController {
                     houseImgCustom.setHouseID(houseCustom1.getHouseID());
                     //houseImgService.insert(houseImgCustom);
                 }
-
-                for(int i = 0;i<houseParts.size();i++){
-                    HousePartsCustom housePartsCustom = new HousePartsCustom();
-                   if(houseParts.get(i).get("name").equals("WIFI")) {
-                        if(houseParts.get(i).get("checked") == "true"){
-                            housePartsCustom.setWifi(1);
-                        }
-                   }
-                }
+                System.out.println(houseParts);
+//                for(int i = 0;i<houseParts.size();i++){
+//                    HousePartsCustom housePartsCustom = new HousePartsCustom();
+//                   if(houseParts.get(i).get("name").equals("WIFI")) {
+//                        if(houseParts.get(i).get("checked") == "true"){
+//                            housePartsCustom.setWifi(1);
+//                        }
+//                   }
+//                }
                 return new Message("1","成功");
 
             }catch (Exception e){
